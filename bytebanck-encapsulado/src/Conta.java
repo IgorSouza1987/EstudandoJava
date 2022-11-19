ec92e1a82667cb269fa98165505bc12987af9653
iaraujonunes <iaraujonunes@BR-PE064ABB>
2022-11-19 04:10:17 -0300

    Initial commit

---
+++ bytebanck-encapsulado/src/Conta.java
@@ -0,0 +1,77 @@
+public class Conta {
+	private double	saldo;
+	private int	agencia;
+	private int	numero;
+	private Cliente	titular;
+	private static int total;
+	
+	
+	
+	public Conta(int agencia,int numero) {
+		total++;
+		this.agencia = agencia;
+		this.numero = numero;
+		 System.out.println("O total de contas é :" +total);
+		
+	}
+	 public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	 
+	 public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo-=valor;
+			return true;
+		}
+		else
+			return false;
+	}
+	 
+	 public boolean transfere(double valor , Conta destino) {
+		 if (this.saldo >= valor) {
+			this.saldo  -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		 else
+			return false;
+	 }
+
+	 public double getSaldo() {
+		 return this.saldo;
+	 }
+	 
+	 public int getNumero() {
+		 return this.numero;
+	 }
+	 
+	 public void setNumero(int novoNumero) {
+		 this.numero = novoNumero;
+	 }
+	 
+	 public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	 
+	 public int getAgencia() {
+		return this.agencia;
+	}
+	 
+	 public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	 
+	 public Cliente getTitular() {
+		return this.titular;
+	}
+	 
+	 public static int getTotal() {
+		return total;
+	}
+}
+
+	 
+
+
+
+
---
+++ bytebanck-encapsulado/src/TestaValores.java
@@ -0,0 +1,13 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		
+		Conta conta1 = new Conta(1332,23322);
+		Conta conta2 = new Conta(1322,23122);
+		Conta conta3 = new Conta(1342,23422);
+		
+		
+		System.out.println(Conta.getTotal());
+	}
+}
